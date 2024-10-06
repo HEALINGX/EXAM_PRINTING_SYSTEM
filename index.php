3a5d20cd49119817dae4b172db4242bb906ef5e3
Pxatric <chyannyddc23@gmail.com>
2024-10-06 14:28:40 +0700

    🐶Login Complete100%

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $dbname = "printing_exam";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -18,17 +18,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Prepare and bind
+   
     $stmt = $conn->prepare("SELECT user_id, user_password, user_role FROM User WHERE user_email = ?");
     $stmt->bind_param("s", $email);
     $stmt->execute();
-    $stmt->store_result(); // Store the result for checking the number of rows
+    $stmt->store_result(); 
     $stmt->bind_result($user_id, $hashed_password, $user_role);
     $stmt->fetch();
 
-    // Check if user exists and password is correct using md5
+ 
     if ($stmt->num_rows > 0 && md5($password) == $hashed_password) {
-        // Start a new session
+       
         $_SESSION["user_id"] = $user_id;
         $_SESSION["email"] = $email;
 
@@ -65,20 +65,21 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginstyles.css"> 
     <title>Login</title>
 </head>
 <body>
-    <h2>Login Page</h2>
     <form action="index.php" method="post">
+        <h2>Sign In</h2>
         <div>
-            <label for="email">Email:</label>
+            <label for="email">E-mail</label>
             <input type="email" id="email" name="email" required>
         </div>
         <div>
-            <label for="password">Password:</label>
+            <label for="password">Password</label>
             <input type="password" id="password" name="password" required>
         </div>
-        <button type="submit">Login</button>
+        <button type="submit">Sign In</button>
     </form>
 </body>
 </html>
